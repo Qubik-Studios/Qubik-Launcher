b62452a156b067fab24b8163e52b55d7d70de324
ConmineLP <conminelp321@gmail.com>
2021-03-14 04:18:05 +0100

    Working on  new MOTD system

--- app/assets/js/discordwrapper.js
+++ app/assets/js/discordwrapper.js
@@ -1,18 +1,35 @@
+const http = require('http')
+const fs = require('fs')
 const logger = require('./loggerutil')('%c[DiscordWrapper]', 'color: #7289da; font-weight: bold')
+const motdlogger = require('./loggerutil')('%c[Discord-MOTD]', 'color: #7289da; font-weight: bold')
 
 const RPC = require('discord-rpc')
 const rpc = new RPC.Client({
     transport: 'ipc'
 })
 
+
 // MOTD System
 
+/*
 let arr = ['Made in Germany!', 'YEET!', 'Qubik Client', 'BETA!', 'Who has cake?', 'You shouldn\'t see this...', 'BETA!', 'NULL', 'BETA!', 'Maxthier was here', 'ConmineLP was here', 'You arent here...', 'BETA!']
-const motd = arr[Math.floor(Math.random() * arr.length)]
-logger.log('Current MOTD-List: ' + arr)
-logger.log('Current MOTD: ' + motd)
+*/
 
-logger.log('RPC Started')
+const file = fs.createWriteStream(process.env.APPDATA + '/Qubik Launcher/motd.txt')
+http.get('http://data.qubik-studios.net/data/QubikClient/motd.txt', response => {response.pipe(file)})
+
+let data = fs.createReadStream(process.env.APPDATA + '/Qubik Launcher/motd.txt')
+let fileData = fs.readFileSync(process.env.APPDATA + '/Qubik Launcher/motd.txt',{encoding:'utf8'});
+motdlogger.log(fileData)
+
+
+let motdraw = ['TEMP']
+const motd = motdraw[Math.floor(Math.random() * motdraw.length)]
+
+motdlogger.log('Current MOTD-List: ' + motdraw)
+motdlogger.log('Current MOTD: ' + motd)
+
+logger.log('RPC Init')
 
 rpc.on('ready', () => {
     rpc.setActivity({
@@ -25,9 +42,10 @@ rpc.on('ready', () => {
         smallImageText: 'v0.1.1-Beta.0'
     })
 
-    logger.log('Rich presence ready!')
+    logger.log('RPC Init finished..')
 })
 
 rpc.login({
     clientId: '806600071088308224'
 })
+logger.log('RPC Started!')
